f33f8d57c4a1258f8a83acf3415b460117f950cb
vita <vita.rosenberg@lemonade.com>
2022-07-20 12:15:51 +0300

    Update pet_finance_sanity_QA.sql

--- pet_finance_sanity_QA.sql
+++ pet_finance_sanity_QA.sql
@@ -24,19 +24,19 @@ having ROUND(SUM(monthly_earned_premium),2) < (-0.01);
 
 --pet Flat canclled with written or earned <> 0
 WITH pet_flat_cancelled AS(
-    SELECT b.public_id AS policyId
-    FROM billing.finance_events a 
-    JOIN pet.policies b ON a.id=b.id
-    where activity = 'policy_cancellation' 
+    SELECT entity_id AS policyId
+    FROM billing.finance_events activity
+    where activity= 'policy_cancellation' 
     and metadata:flat='true'
 )
 SELECT sum(monthly_written_premium), public_id, 'monthly_written_premium' type
 from identifier($tableName)
 where public_id in(select policyId from pet_flat_cancelled)
 group by public_id
-having SUM(monthly_written_premium)  <> 0
+having ABS(SUM(monthly_written_premium))  > 0.01
 UNION
 select sum(monthly_earned_premium), public_id,'monthly_earned_premium' type
 from identifier($tableName)
 where public_id in(select policyId from pet_flat_cancelled)
 group by public_id
+having ABS(SUM(monthly_earned_premium)) > 0.01 
