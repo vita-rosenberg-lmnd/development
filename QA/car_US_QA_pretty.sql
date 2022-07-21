f250a993d97080a638447dcfa54d51e3b6536a0b
vita <vita.rosenberg@lemonade.com>
2022-07-21 18:03:55 +0300

    Update car_US_QA_pretty.sql

--- QA/car_US_QA_pretty.sql
+++ QA/car_US_QA_pretty.sql
@@ -1,115 +1,186 @@
---set tableName = 'CAR_FINANCE.tmpPREMIUM_REPORT_US';
---create or replace temporary table identifier($tableName)
---AS
---select * from CAR_FINANCE.PREMIUM_REPORT_US;
 
---car Not canclled with written or earned = 0
-with car_not_cancelled AS(
-    SELECT public_id
-    from car.policies
-    where cancelled_at is null
+WITH temp_premium_report_us AS(
+    select * from CAR_FINANCE.PREMIUM_REPORT_US
 ),
+
+car_not_cancelled AS(
+       SELECT public_id
+       FROM   car.policies
+       WHERE  cancelled_at IS NULL 
+), 
+
 car_not_cancelled_monthly_written_premium AS(
-select sum(monthly_written_premium) AS sum_of_type, public_id, 
-    'car_not_cancelled_monthly_written_premium' AS errType
-from identifier($tableName)
-where public_id in(select public_id from car_not_cancelled)
-group by public_id
-having (-0.01) < SUM(monthly_written_premium) and SUM(monthly_written_premium) < (0.01))
-,
+         SELECT   SUM(monthly_written_premium) AS sum_of_type,
+                  public_id,
+                  'car_not_cancelled_monthly_written_premium' AS errType
+         FROM     temp_premium_report_us
+         WHERE    public_id IN(
+                         SELECT public_id
+                         FROM   car_not_cancelled)
+         GROUP BY public_id
+         HAVING   (-0.01) < SUM(monthly_written_premium)
+                  AND SUM(monthly_written_premium) < (0.01)
+), 
+
 car_not_cancelled_monthly_earned_premium AS(
-select sum(monthly_earned_premium) AS sum_of_type, public_id, 
-    'car_not_cancelled_monthly_earned_premium' AS errType
-from identifier($tableName)
-where public_id in(select public_id from car_not_cancelled)
-group by public_id
-having (-0.01) < SUM(monthly_earned_premium) and SUM(monthly_earned_premium) < (0.01)
+         SELECT   SUM(monthly_earned_premium) AS sum_of_type,
+                  public_id,
+                  'car_not_cancelled_monthly_earned_premium' AS errType
+         FROM     temp_premium_report_us
+         WHERE    public_id IN(
+                         SELECT public_id
+                         FROM   car_not_cancelled)
+         GROUP BY public_id
+         HAVING   (-0.01) < SUM(monthly_earned_premium)
+         AND      SUM(monthly_earned_premium) < (0.01) 
 ),
+
 --car Flat canclled with written or earned <> 0
 car_flat_cancelled AS(
-    SELECT entity_id AS policyId
-    FROM billing.finance_events activity
-    where activity= 'policy_cancellation' 
-    and metadata:flat='true'
-    and entity_id like 'LCP%'
-),
+       SELECT entity_id AS policyId
+       FROM   billing.finance_events activity
+       WHERE  activity= 'policy_cancellation'
+       AND    metadata:flat='true'
+       AND    entity_id LIKE 'LCP%' 
+), 
+
 car_flat_cancelled_monthly_written_premium AS(
-SELECT sum(monthly_written_premium), public_id, 'car_flat_cancelled_monthly_written_premium' errType
-from identifier($tableName)
-where public_id in(select policyId from car_flat_cancelled)
-group by public_id
-having ABS(SUM(monthly_written_premium))  > 0.01
-    ),
-car_flat_cancelled_monthly_earned_premium AS(
-select sum(monthly_earned_premium), public_id,'car_flat_cancelled_monthly_earned_premium' errType
-from identifier($tableName)
-where public_id in(select policyId from car_flat_cancelled)
-group by public_id
-having ABS(SUM(monthly_earned_premium)) > 0.01 
+         SELECT   SUM(monthly_written_premium),
+                  public_id,
+                  'car_flat_cancelled_monthly_written_premium' errType
+         FROM     temp_premium_report_us
+         WHERE    public_id IN(
+                         SELECT policyId
+                         FROM   car_flat_cancelled)
+         GROUP BY public_id
+         HAVING   ABS(SUM(monthly_written_premium)) > 0.01 
 ),
+
+car_flat_cancelled_monthly_earned_premium AS(
+         SELECT   SUM(monthly_earned_premium),
+                  public_id,
+                  'car_flat_cancelled_monthly_earned_premium' errType
+         FROM     temp_premium_report_us
+         WHERE    public_id IN(
+                         SELECT policyId
+                         FROM   car_flat_cancelled)
+         GROUP BY public_id
+         HAVING   ABS(SUM(monthly_earned_premium)) > 0.01 
+),
+
 --monthly_unearned_premium < 0
 monthly_unearned_premium AS(
-select SUM(monthly_unearned_premium), public_id, 'monthly_unearned_premium < 0' AS errType
-from identifier($tableName)
-group by public_id
-having ROUND(SUM(monthly_unearned_premium),2) < (-0.01)
+         SELECT   SUM(monthly_unearned_premium),
+                  public_id,
+                  'monthly_unearned_premium < 0' AS errType
+         FROM     temp_premium_report_us
+         GROUP BY public_id
+         HAVING   ROUND(SUM(monthly_unearned_premium),2) < (-0.01) 
 ),
+
 --monthly_earned_premium < 0
 monthly_earned_premium AS(
-select SUM(monthly_earned_premium), public_id, 'monthly_earned_premium < 0' AS errType
-from identifier($tableName)
-group by public_id
-having ROUND(SUM(monthly_earned_premium),2) < (-0.01)
+         SELECT   SUM(monthly_earned_premium),
+                  public_id,
+                  'monthly_earned_premium < 0' AS errType
+         FROM     temp_premium_report_us($tableName)
+         GROUP BY public_id
+         HAVING   ROUND(SUM(monthly_earned_premium),2) < (-0.01) 
 ),
+
 --monthly_written_premium < 0
 monthly_written_premium AS(
-select SUM(monthly_written_premium), public_id, 'monthly_written_premium < 0' AS errType
-from identifier($tableName)
-group by public_id
-having ROUND(SUM(monthly_written_premium),2) < (-0.01)
+         SELECT   SUM(monthly_written_premium),
+                  public_id,
+                  'monthly_written_premium < 0' AS errType
+         FROM     temp_premium_report_us
+         GROUP BY public_id
+         HAVING   ROUND(SUM(monthly_written_premium),2) < (-0.01) 
 ),
---car Policy is active and written or earned <= 0  
+
+--car Policy is active and written or earned <= 0
 car_active_policies AS(
-select public_id 
-from car.policies
-where status = 'active'
-),
+       SELECT public_id
+       FROM   car.policies
+       WHERE  status = 'active' 
+), 
+
 car_active_policies_monthly_written_premium AS(
-select sum(monthly_written_premium), public_id,'car_active_policies_monthly_written_premium' AS errType
-from identifier($tableName)
-where public_id in(select public_id from car_active_policies)
-group by public_id
-having SUM(monthly_written_premium)  <= 0
+         SELECT   SUM(monthly_written_premium),
+                  public_id,
+                  'car_active_policies_monthly_written_premium' AS errType
+         FROM     temp_premium_report_us
+         WHERE    public_id IN(
+                         SELECT public_id
+                         FROM   car_active_policies)
+         GROUP BY public_id
+         HAVING   SUM(monthly_written_premium) <= 0 
 ),
+
 car_active_policies_monthly_earned_premium AS(
-select sum(monthly_earned_premium), public_id,'car_active_policies_monthly_earned_premium' AS errType
-from identifier($tableName)
-where public_id in(select public_id from car_active_policies)
-group by public_id
-having SUM(monthly_earned_premium) <= 0
-),
+         SELECT   SUM(monthly_earned_premium),
+                  public_id,
+                  'car_active_policies_monthly_earned_premium' AS errType
+         FROM     temp_premium_report_us
+         WHERE    public_id IN(
+                         SELECT public_id
+                         FROM   car_active_policies)
+         GROUP BY public_id
+         HAVING   SUM(monthly_earned_premium) <= 0 )
+,
+
 --car Policy is not active and written <> earned
 car_inactive_policies AS(
-    SELECT public_id
-    from car.policies
-    where status<>'active'
-),
-car_inactive_policies_monthly_difference as(
-    select  (sum(monthly_written_premium) - sum(monthly_earned_premium)) AS monthly_sum, public_id,
-    'car_inactive_policies_monthly_difference' AS errType
-    from identifier($tableName)
-    where public_id in(select public_id from car_inactive_policies)
-    group by public_id
-    having round(ABS((sum(monthly_written_premium) - sum(monthly_earned_premium))),2) > 0.01
-    )
-    
-      SELECT public_id, errType FROM car_not_cancelled_monthly_written_premium
-UNION SELECT public_id, errType FROM car_flat_cancelled_monthly_written_premium
-UNION SELECT public_id, errType FROM car_flat_cancelled_monthly_earned_premium
-UNION SELECT public_id, errType FROM monthly_unearned_premium
-UNION SELECT public_id, errType FROM monthly_earned_premium
-UNION SELECT public_id, errType FROM monthly_written_premium
-UNION SELECT public_id, errType FROM car_active_policies_monthly_written_premium
-UNION SELECT public_id, errType FROM car_active_policies_monthly_earned_premium
-UNION SELECT public_id, errType FROM car_inactive_policies_monthly_difference
-order by errType
+       SELECT public_id
+       FROM   car.policies
+       WHERE  status<>'active' 
+), 
+
+car_inactive_policies_monthly_difference AS(
+         SELECT   (SUM(monthly_written_premium) - SUM(monthly_earned_premium)) AS monthly_sum,
+                  public_id,
+                  'car_inactive_policies_monthly_difference' AS errType
+         FROM     temp_premium_report_us
+         WHERE    public_id IN(
+                         SELECT public_id
+                         FROM   car_inactive_policies)
+         GROUP BY public_id
+         HAVING   ROUND(ABS((SUM(monthly_written_premium) - SUM(monthly_earned_premium))),2) > 0.01 
+)
+
+SELECT public_id,
+       errType
+FROM   car_not_cancelled_monthly_written_premium
+UNION
+SELECT public_id,
+       errType
+FROM   car_flat_cancelled_monthly_written_premium
+UNION
+SELECT public_id,
+       errType
+FROM   car_flat_cancelled_monthly_earned_premium
+UNION
+SELECT public_id,
+       errType
+FROM   monthly_unearned_premium
+UNION
+SELECT public_id,
+       errType
+FROM   monthly_earned_premium
+UNION
+SELECT public_id,
+       errType
+FROM   monthly_written_premium
+UNION
+SELECT public_id,
+       errType
+FROM   car_active_policies_monthly_written_premium
+UNION
+SELECT public_id,
+       errType
+FROM   car_active_policies_monthly_earned_premium
+UNION
+SELECT   public_id,
+         errType
+FROM     car_inactive_policies_monthly_difference
+ORDER BY errType
