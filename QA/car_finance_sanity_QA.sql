5ad7ddb8faeb47556aec44942bdadae13b20c4b9
vita <vita.rosenberg@lemonade.com>
2022-07-21 13:52:56 +0300

    Update car_finance_sanity_QA.sql

--- QA/car_finance_sanity_QA.sql
+++ QA/car_finance_sanity_QA.sql
@@ -7,7 +7,7 @@ create or replace temporary table identifier($tableName)
 AS
 select * from CAR_FINANCE.PREMIUM_REPORT_US
 
---Not canclled with written or earned = 0
+--car Not canclled with written or earned = 0
 with not_cancelled AS(
     SELECT public_id
     from car.policies
@@ -24,3 +24,22 @@ from identifier($tableName)
 where public_id in(select public_id from not_cancelled)
 group by public_id
 having (-0.01) < SUM(monthly_earned_premium) and SUM(monthly_earned_premium) < (0.01)
+
+--pet Flat canclled with written or earned <> 0
+WITH pet_flat_cancelled AS(
+    SELECT entity_id AS policyId
+    FROM billing.finance_events activity
+    where activity= 'policy_cancellation' 
+    and metadata:flat='true'
+)
+SELECT sum(monthly_written_premium), public_id, 'monthly_written_premium' type
+from identifier($tableName)
+where public_id in(select policyId from pet_flat_cancelled)
+group by public_id
+having ABS(SUM(monthly_written_premium))  > 0.01
+UNION
+select sum(monthly_earned_premium), public_id,'monthly_earned_premium' type
+from identifier($tableName)
+where public_id in(select policyId from pet_flat_cancelled)
+group by public_id
+having ABS(SUM(monthly_earned_premium)) > 0.01 
