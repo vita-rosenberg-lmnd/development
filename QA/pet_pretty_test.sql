eea9b6670567ad0f08ed55b69dab9bc51963b700
vita <vita.rosenberg@lemonade.com>
2022-07-26 13:18:20 +0300

    Create pet_pretty_test.sql

---
+++ QA/pet_pretty_test.sql
@@ -0,0 +1,303 @@
+WITH temp_premium_report_us AS(
+  select 
+    * 
+  FROM 
+    car_finance.premium_report_us
+), 
+
+car_not_cancelled AS(
+  SELECT 
+    public_id 
+  FROM 
+    car.policies 
+  WHERE 
+    cancelled_at IS NULL
+), 
+
+car_not_cancelled_monthly_written_premium AS(
+  SELECT 
+    SUM(monthly_written_premium) AS sum_of_type, 
+    public_id, 
+    'Not canclled with written or earned = 0' AS errType 
+  FROM 
+    temp_premium_report_us 
+  WHERE 
+    public_id IN(
+      SELECT 
+        public_id 
+      FROM 
+        car_not_cancelled
+    ) 
+  GROUP BY 
+    public_id 
+  HAVING 
+    SUM(monthly_written_premium) = 0
+), 
+
+car_not_cancelled_monthly_earned_premium AS(
+  SELECT 
+    SUM(monthly_earned_premium) AS sum_of_type, 
+    public_id, 
+    'Not canclled with written or earned = 0' AS errType 
+  FROM 
+    temp_premium_report_us 
+  WHERE 
+    public_id IN(
+      SELECT 
+        public_id 
+      FROM 
+        car_not_cancelled
+    ) 
+  GROUP BY 
+    public_id 
+  HAVING 
+    SUM(monthly_earned_premium) = 0
+), 
+
+--car Flat canclled with written or earned <> 0
+car_flat_cancelled AS(
+  SELECT 
+    entity_id AS policyId 
+  FROM 
+    billing.finance_events activity 
+  WHERE 
+    activity = 'policy_cancellation' 
+    AND metadata : flat = 'true' 
+    AND entity_id LIKE 'LCP%'
+), 
+
+car_flat_cancelled_monthly_written_premium AS(
+  SELECT 
+    SUM(monthly_written_premium) AS sum_of_type, 
+    public_id, 
+    'Flat canclled with written or earned <> 0' errType 
+  FROM 
+    temp_premium_report_us 
+  WHERE 
+    public_id IN(
+      SELECT 
+        policyId 
+      FROM 
+        car_flat_cancelled
+    ) 
+  GROUP BY 
+    public_id 
+  HAVING 
+    ROUND(SUM(monthly_written_premium), 4) <> 0
+), 
+
+car_flat_cancelled_monthly_earned_premium AS(
+  SELECT 
+    SUM(monthly_earned_premium) AS sum_of_type, 
+    public_id, 
+    'Flat canclled with written or earned <> 0' errType 
+  FROM 
+    temp_premium_report_us 
+  WHERE 
+    public_id IN(
+      SELECT 
+        policyId 
+      FROM 
+        car_flat_cancelled
+    ) 
+  GROUP BY 
+    public_id 
+  HAVING 
+        ROUND(SUM(monthly_earned_premium), 4) <> 0
+), 
+
+--monthly_unearned_premium < 0
+monthly_unearned_premium AS(
+  SELECT 
+    SUM(monthly_unearned_premium) AS sum_of_type, 
+    public_id, 
+    'Monthly unearned premium < 0' AS errType 
+  FROM 
+    temp_premium_report_us 
+  GROUP BY 
+    public_id 
+  HAVING 
+    ROUND(SUM(monthly_unearned_premium), 4) < 0
+), 
+
+--monthly_earned_premium < 0
+monthly_earned_premium AS(
+  SELECT 
+    SUM(monthly_earned_premium) AS sum_of_type, 
+    public_id, 
+    'Monthly earned premium < 0' AS errType 
+  FROM 
+    temp_premium_report_us 
+  GROUP BY 
+    public_id 
+  HAVING 
+    ROUND(SUM(monthly_earned_premium), 4) < 0
+), 
+
+--monthly_written_premium < 0
+monthly_written_premium AS(
+  SELECT 
+    SUM(monthly_written_premium) AS sum_of_type, 
+    public_id, 
+    'Monthly written premium < 0' AS errType 
+  FROM 
+    temp_premium_report_us 
+  GROUP BY 
+    public_id 
+  HAVING 
+    ROUND(SUM(monthly_written_premium), 4) < 0
+), 
+
+--car Policy is active and written or earned <= 0
+car_active_policies AS(
+  SELECT 
+    public_id 
+  FROM 
+    car.policies 
+  WHERE 
+    status = 'active'
+), 
+
+car_active_policies_monthly_written_premium AS(
+  SELECT 
+    SUM(monthly_written_premium) AS sum_of_type, 
+    public_id, 
+    'Policy is active and written or earned <= 0 ' AS errType 
+  FROM 
+    temp_premium_report_us 
+  WHERE 
+    public_id IN(
+      SELECT 
+        public_id 
+      FROM 
+        car_active_policies
+    ) 
+  GROUP BY 
+    public_id 
+  HAVING 
+    SUM(monthly_written_premium) <= 0
+), 
+
+car_active_policies_monthly_earned_premium AS(
+  SELECT 
+    SUM(monthly_earned_premium) AS sum_of_type, 
+    public_id, 
+    'Policy is active and written or earned <= 0 ' AS errType 
+  FROM 
+    temp_premium_report_us 
+  WHERE 
+    public_id IN(
+      SELECT 
+        public_id 
+      FROM 
+        car_active_policies
+    ) 
+  GROUP BY 
+    public_id 
+  HAVING 
+    ROUND(SUM(monthly_earned_premium), 4) <= 0
+),
+
+--car Policy is not active and written <> earned
+car_inactive_policies AS(
+  SELECT 
+    public_id 
+  FROM 
+    car.policies 
+  WHERE 
+    status <> 'active'
+), 
+
+car_inactive_policies_monthly_difference AS(
+  SELECT 
+    (
+      ROUND((SUM(monthly_written_premium) - SUM(monthly_earned_premium)), 4)
+    ) AS sum_of_type, 
+    public_id, 
+    'Policy is not active and written <> earned' AS errType 
+  FROM 
+    temp_premium_report_us 
+  WHERE 
+    public_id IN(
+      SELECT 
+        public_id 
+      FROM 
+        car_inactive_policies
+    ) 
+  GROUP BY 
+    public_id 
+  HAVING 
+    ROUND((SUM(monthly_written_premium) - SUM(monthly_earned_premium)), 4) <> 0
+) 
+SELECT 
+  public_id, 
+  errType,
+  sum_of_type
+FROM 
+  car_not_cancelled_monthly_written_premium 
+UNION 
+SELECT 
+  public_id, 
+  errType,
+  sum_of_type
+FROM 
+  car_not_cancelled_monthly_earned_premium 
+UNION
+SELECT 
+  public_id, 
+  errType,
+  sum_of_type
+FROM 
+  car_flat_cancelled_monthly_written_premium 
+UNION 
+SELECT 
+  public_id, 
+  errType,
+  sum_of_type
+FROM 
+  car_flat_cancelled_monthly_earned_premium 
+UNION 
+SELECT 
+  public_id, 
+  errType,
+  sum_of_type
+FROM 
+  monthly_unearned_premium
+UNION 
+SELECT 
+  public_id, 
+  errType,
+  sum_of_type
+FROM 
+  monthly_earned_premium 
+UNION 
+SELECT 
+  public_id, 
+  errType,
+  sum_of_type
+FROM 
+  monthly_written_premium 
+UNION 
+SELECT 
+  public_id, 
+  errType,
+  sum_of_type
+FROM 
+  car_active_policies_monthly_written_premium 
+UNION 
+SELECT 
+  public_id, 
+  errType,
+  sum_of_type
+FROM 
+  car_active_policies_monthly_earned_premium 
+UNION 
+SELECT 
+  public_id, 
+  errType,
+  sum_of_type 
+FROM 
+  car_inactive_policies_monthly_difference 
+ORDER BY 
+  errType,
+  sum_of_type
